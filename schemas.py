245d480755d76688d20d50f543002cd145e51275
lucascost <lcyancosta@gmail.com>
2023-03-22 19:01:37 -0300

    add: ListType schema

--- schemas.py
+++ schemas.py
@@ -1,15 +1,27 @@
 from pydantic import BaseModel
 
 
+# LIST
+
 class ListBase(BaseModel):
     title: str
 
-
 class ListCreate(ListBase):
-    pass
-
+    type_id: int
 
 class List(ListBase):
+    id: int
+    type_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# LIST_TYPE
+class ListTypeBase(BaseModel):
+    name: str
+
+class ListType(ListTypeBase):
     id: int
 
     class Config:
