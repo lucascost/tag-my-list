a9510d92084d5fbf8ec150c5f9f7c266c61465f3
lucascost <lcyancosta@gmail.com>
2023-03-27 18:53:39 -0300

    add: Tag model and relationship

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ class List(Base):
     title = Column(String, unique=True, index=True)
     type_id = Column(Integer, ForeignKey("list_types.id", ondelete="CASCADE"))
     type = relationship("ListType")
+    tags = relationship("Tag", back_populates="list")
 
 
 class ListType(Base):
@@ -18,3 +19,12 @@ class ListType(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
+
+
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    list_id = Column(Integer, ForeignKey("lists.id", ondelete="CASCADE"))
+    list = relationship("List", back_populates="tags")
