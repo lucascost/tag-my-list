09d1b41a5e337ee149e2d043ba20512a77b8d731
lucascost <lcyancosta@gmail.com>
2023-03-27 18:54:44 -0300

    add: Tag schemas

--- schemas.py
+++ schemas.py
@@ -6,12 +6,15 @@ from pydantic import BaseModel
 class ListBase(BaseModel):
     title: str
 
+
 class ListCreate(ListBase):
     type_id: int
+
 
 class List(ListBase):
     id: int
     type_id: int
+    tags: list['Tag'] = []
 
     class Config:
         orm_mode = True
@@ -21,7 +24,24 @@ class List(ListBase):
 class ListTypeBase(BaseModel):
     name: str
 
+
 class ListType(ListTypeBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# TAG
+class TagBase(BaseModel):
+    name: str
+
+
+class TagCreate(TagBase):
+    list_id: int
+
+
+class Tag(TagBase):
     id: int
 
     class Config:
