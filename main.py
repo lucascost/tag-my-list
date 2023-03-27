6c58111db531c0a960c5b22d70b75db21174940e
lucascost <lcyancosta@gmail.com>
2023-03-27 18:58:59 -0300

    add: Tag endpoints; remove: read_listtype(); fix: read_lists() show 'tags'

--- main.py
+++ main.py
@@ -31,13 +31,16 @@ def read_list(list_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="List not found")
     return serialize_list(db_list)
 
-@app.get("/all", response_model=list[serializers.ListSerializer])
+
+@app.get("/", response_model=list[serializers.ListSerializer])
 def read_lists(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     lists = crud.get_lists(db, skip, limit)
     db_lists = []
     for list in lists:
+        list.tags = []
         db_lists.append(serialize_list(list))
     return db_lists
+
 
 @app.post("/", response_model=serializers.ListSerializer)
 def create_list(list: schemas.ListCreate, db: Session = Depends(get_db)):
@@ -49,13 +52,23 @@ def create_list(list: schemas.ListCreate, db: Session = Depends(get_db)):
 
 
 # LIST_TYPES
-@app.get("/listtypes/{listtype_id}", response_model=schemas.ListType)
-def get_listtype(listtype_id: int, db: Session = Depends(get_db)):
-    db_listtype = crud.get_list_type(db, listtype_id)
-    if db_listtype is None:
-        raise HTTPException(status_code=400, detail="ListType not found")
-    return db_listtype
 
 @app.get("/listtypes/", response_model=list[schemas.ListType])
-def get_listtypes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_listtypes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_list_types(db, skip, limit)
+
+
+# TAG
+@app.get("/lists/{list_id}/tags", response_model=list[schemas.Tag])
+def read_tags_by_list(list_id: int, db: Session = Depends(get_db)):
+    db_list = crud.get_list(db, list_id)
+    if db_list is None:
+        raise HTTPException(status_code=400, detail="List not found")
+    db_tags = crud.get_tags_by_list(db, list_id)
+    return db_tags
+
+
+@app.post("/lists/{list_id}/tags", response_model=schemas.Tag)
+def create_tag(list_id: int, tag: schemas.TagCreate, db: Session = Depends(get_db)):
+    list = read_list(list_id, db)
+    return crud.create_tag(db, list['id'], tag)
