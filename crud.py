d56496c9755df2acca340c86d48b11f8857404e0
lucascost <lcyancosta@gmail.com>
2023-03-27 18:51:42 -0300

    add: Tag crud functions; remove: ListType get_list_type()

--- crud.py
+++ crud.py
@@ -2,8 +2,10 @@ from typing import Union
 
 from sqlalchemy.orm import Session
 
+from models import Tag
 import models
 import schemas
+
 
 # LIST
 def get_list(db: Session, list_info: Union[int, str]):
@@ -12,8 +14,10 @@ def get_list(db: Session, list_info: Union[int, str]):
 
     return db.query(models.List).filter(models.List.title == list_info).first()
 
+
 def get_lists(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.List).offset(skip).limit(limit).all()
+
 
 def create_list(db: Session, list: schemas.ListCreate):
     db_list = models.List(title=list.title, type_id=list.type_id)
@@ -24,8 +28,19 @@ def create_list(db: Session, list: schemas.ListCreate):
 
 
 # LIST_TYPE
-def get_list_type(db: Session, list_type_id: int):
-    return db.query(models.ListType).filter(models.ListType.id == list_type_id).first()
-
 def get_list_types(db: Session, skip: int = 0, limit: int = 3):
     return db.query(models.ListType).offset(skip).limit(limit).all()
+
+
+# TAG
+def get_tags_by_list(db: Session, list_id: int):
+    return db.query(Tag).filter(models.Tag.list_id == list_id).all()
+
+
+def create_tag(db: Session, list_id: int, tag: schemas.TagCreate):
+    db_list = get_list(db, list_id)
+    new_tag = models.Tag(name=tag.name, list_id=db_list.id)
+    db.add(new_tag)
+    db.commit()
+    db.refresh(new_tag)
+    return new_tag
