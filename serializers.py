a9fdd9041a091a3530953d1b738b82d247d5c537
lucascost <lcyancosta@gmail.com>
2023-03-27 19:00:49 -0300

    update: 'tags' included in ListSerializer

--- serializers.py
+++ serializers.py
@@ -1,19 +1,23 @@
 from pydantic import BaseModel
 
 from models import List
+from schemas import Tag
 
 
 class ListSerializer(BaseModel):
     id: int
     title: str
     type: str
+    tags: list[Tag]
 
     class Config:
         orm_mode = True
+
 
 def serialize_list(list: List):
     return {
         'id': list.id,
         'title': list.title,
-        'type': list.type.name
+        'type': list.type.name,
+        'tags': list.tags
     }
